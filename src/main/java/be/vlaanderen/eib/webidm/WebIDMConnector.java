2848c2a142fae8489dd7c25ffcd1e0159ceefdd8
Francis Vermeulen <50039430@HCBECND63101BZ.be.hpcds.net>
2018-04-04 09:38:43 +0200

    Groot aantal CO's

--- src/main/java/be/vlaanderen/eib/webidm/WebIDMConnector.java
+++ src/main/java/be/vlaanderen/eib/webidm/WebIDMConnector.java
@@ -18,16 +18,20 @@ package be.vlaanderen.eib.webidm;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.identityconnectors.common.logging.Log;
+import org.identityconnectors.framework.common.objects.Attribute;
 import org.identityconnectors.framework.common.objects.AttributeBuilder;
 import org.identityconnectors.framework.common.objects.AttributeInfo;
 import org.identityconnectors.framework.common.objects.AttributeInfoBuilder;
 import org.identityconnectors.framework.common.objects.ConnectorObject;
 import org.identityconnectors.framework.common.objects.ConnectorObjectBuilder;
+import org.identityconnectors.framework.common.objects.Name;
 import org.identityconnectors.framework.common.objects.ObjectClass;
 import org.identityconnectors.framework.common.objects.ObjectClassInfo;
 import org.identityconnectors.framework.common.objects.ObjectClassInfoBuilder;
@@ -40,6 +44,7 @@ import org.identityconnectors.framework.common.objects.SyncDeltaType;
 import org.identityconnectors.framework.common.objects.SyncResultsHandler;
 import org.identityconnectors.framework.common.objects.SyncToken;
 import org.identityconnectors.framework.common.objects.Uid;
+import org.identityconnectors.framework.common.objects.filter.EqualsFilter;
 import org.identityconnectors.framework.common.objects.filter.Filter;
 import org.identityconnectors.framework.common.objects.filter.FilterTranslator;
 import org.identityconnectors.framework.spi.Configuration;
@@ -57,10 +62,12 @@ import be.vlaanderen.eib.webidm.query.WebIDMFilterTranslator;
 public class WebIDMConnector implements Connector, SchemaOp, SearchOp<Filter>, SyncOp, DeleteOp, TestOp {
 
     private static final Log LOG = Log.getLog(WebIDMConnector.class);
+    private static final int AANTAL_OBJECTEN = 100;
 
     private WebIDMConfiguration configuration;
     private WebIDMConnection connection;
     private static Integer vorigeTokenWaarde;
+    private static Map<String, ConnectorObject> connectorObjecten = new HashMap<>();
 
     @Override
     public Configuration getConfiguration() {
@@ -97,45 +104,63 @@ public class WebIDMConnector implements Connector, SchemaOp, SearchOp<Filter>, S
 	@Override
 	public void executeQuery(ObjectClass objectClass, Filter query, ResultsHandler handler, OperationOptions options) {
 		
-		LOG.info("Filter translator maken");
+		LOG.info("Query uitvoeren");
 		
 		if (ObjectClass.ACCOUNT.is(objectClass.getObjectClassValue())) {
 
-			for (ConnectorObject connObject : maakIDDObject(objectClass, 10, false)) {
-				handler.handle(connObject);
+			if (isEqualsFilter(query, "__UID__")) {
+				Attribute uuidAttribute = ((EqualsFilter)query).getAttribute();
+	        	String uuid = (String)uuidAttribute.getValue().get(0);
+	        	ConnectorObject connectorObject = connectorObjecten.get(uuid);
+	        	if (connectorObject != null) {
+	        		handler.handle(connectorObject);
+	        	}
+			} else {
+				for (ConnectorObject connObject : maakIDDObjecten(objectClass, 10, false)) {
+					handler.handle(connObject);
+				}
 			}
 		}
 	}
 	
-	private List<ConnectorObject> maakIDDObject(ObjectClass objectClass, int aantal, boolean wijzigen) {
+	private boolean isEqualsFilter(Filter icfFilter, String icfAttrname) {
 		
-		List<ConnectorObject> objecten = new ArrayList<>();
-		int aantalTeMaken = 0;
-		while (aantalTeMaken < aantal) {
-			ConnectorObjectBuilder coBuilder = new ConnectorObjectBuilder();
-			coBuilder.setObjectClass(objectClass);
-			coBuilder.setUid("dc797290-703a-41d3-b4bb-6b3aa4cd5291" + "-" + (aantalTeMaken+1));
-			coBuilder.setName("TestVI" + "-" + (aantalTeMaken+1));
-			
-			AttributeBuilder attributeBuilder = new AttributeBuilder();
-			attributeBuilder.setName("idd_cn");
-			attributeBuilder.addValue("Dierckx, Erwin" + "-" + (aantalTeMaken+1));
-			coBuilder.addAttribute(attributeBuilder.build());
-			attributeBuilder = new AttributeBuilder();
-			attributeBuilder.setName("idd_dn");
-			if (!wijzigen) {
-				attributeBuilder.addValue("vo-idv=dc797290-703a-41d3-b4bb-6b3aa4cd5291,ou=gid,dc=vlaanderen,dc=be");
-			} else {
-				attributeBuilder.addValue("vo-idv=Dierckx,ou=gid,dc=vlaanderen,dc=be");
+		return icfFilter != null && (icfFilter instanceof EqualsFilter) && icfAttrname.equals(((EqualsFilter)icfFilter).getName());
+	}
+	
+	private List<ConnectorObject> maakIDDObjecten(ObjectClass objectClass, int aantal, boolean wijzigen) {
+		
+
+			int aantalTeMaken = 0;
+			while (aantalTeMaken < aantal) {
+				ConnectorObjectBuilder coBuilder = new ConnectorObjectBuilder();
+				coBuilder.setObjectClass(objectClass);
+				String uuid = "dc797290-703a-41d3-b4bb-6b3aa4cd5291" + "-" + (aantalTeMaken+1);
+				coBuilder.setUid(uuid);
+				String name = "TestVI" + "-" + (aantalTeMaken+1);
+				coBuilder.setName(name);
+				
+				AttributeBuilder attributeBuilder = new AttributeBuilder();
+				attributeBuilder.setName("idd_cn");
+				attributeBuilder.addValue("Dierckx, Erwin" + "-" + (aantalTeMaken+1));
+				coBuilder.addAttribute(attributeBuilder.build());
+				attributeBuilder = new AttributeBuilder();
+				attributeBuilder.setName("idd_dn");
+				if (!wijzigen) {
+					attributeBuilder.addValue("vo-idv=dc797290-703a-41d3-b4bb-6b3aa4cd5291,ou=gid,dc=vlaanderen,dc=be");
+				} else {
+					attributeBuilder.addValue("vo-idv=Dierckx,ou=gid,dc=vlaanderen,dc=be");
+				}
+				coBuilder.addAttribute(attributeBuilder.build());
+				attributeBuilder = new AttributeBuilder();
+				attributeBuilder.setName("idd_federatie-applicatie12-autorisatiedata");
+				attributeBuilder.addValue("VO-RA");
+				coBuilder.addAttribute(attributeBuilder.build());
+				connectorObjecten.put(uuid, coBuilder.build());
+				++aantalTeMaken;
 			}
-			coBuilder.addAttribute(attributeBuilder.build());
-			attributeBuilder = new AttributeBuilder();
-			attributeBuilder.setName("idd_federatie-applicatie12-autorisatiedata");
-			attributeBuilder.addValue("VO-RA");
-			coBuilder.addAttribute(attributeBuilder.build());
-			objecten.add(coBuilder.build());
-			++aantalTeMaken;
-		}
+
+		List<ConnectorObject> objecten = new ArrayList<>(connectorObjecten.values());
 		
 		return objecten;
 	}
@@ -200,19 +225,18 @@ public class WebIDMConnector implements Connector, SchemaOp, SearchOp<Filter>, S
 		
 		LOG.info("Live sync");
 		int aantal = 1;
-		int maxAantal = 10;
 		SyncDeltaType operatie = bepaalDeltaType(token);
 		boolean wijzigen = operatie == SyncDeltaType.UPDATE;
-		for (ConnectorObject connObject : maakIDDObject(objectClass, maxAantal, wijzigen)) {
+		for (ConnectorObject connObject : maakIDDObjecten(objectClass, AANTAL_OBJECTEN, wijzigen)) {
 			SyncDeltaBuilder deltaBuilder =  new SyncDeltaBuilder();
 			deltaBuilder.setObjectClass(ObjectClass.ACCOUNT);
 			deltaBuilder.setDeltaType(operatie);
 			deltaBuilder.setObject(connObject);
-			deltaBuilder.setUid(new Uid("dc797290-703a-41d3-b4bb-6b3aa4cd5291" + "-" + aantal));
+			deltaBuilder.setUid(connObject.getUid());
 			deltaBuilder.setToken(token);
 			handler.handle(deltaBuilder.build());
 			++aantal;
-			if (aantal > maxAantal) {
+			if (aantal > AANTAL_OBJECTEN) {
 				break;
 			}
 		}
